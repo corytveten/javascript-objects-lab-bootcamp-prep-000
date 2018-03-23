c0ae9620becadfe80d59576b9ea998bc0ba606d9
corytveten <corytveten@gmail.com>
2018-03-23 10:43:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var recipes = { };
 
 function updateObjectWithKeyAndValue(obj, key, value) {
-  obj.key = "value";
+  return Obj.assign({}, obj, { [key]: value})
+}
 
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
+  return obj.key = value;
   return obj;
 }
 
