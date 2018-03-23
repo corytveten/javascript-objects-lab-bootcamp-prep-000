d1b8853d374a4c03e5ec1d2b0a51148467b612c9
corytveten <corytveten@gmail.com>
2018-03-23 10:55:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function deleteFromObjectByKey(object, key) {
   return newObj;
 }
 
-function destructivelyDeleteFromObjectWithKeyAndValue(object, key) {
+function destructivelyDeleteFromObjectByKey(object, key) {
   delete object.key;
   return object;
 }
